78df1a41e8b6e4e396a7ea02b12d45e08fa4fa53
Gerardo <gerardoijgv11@gmail.com>
2024-01-16 14:39:36 -0600

    Agregué comentarios

--- simplifyPath.py
+++ simplifyPath.py
@@ -1,35 +1,33 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        res = "/"
-        carpetas = path.split('/')
-        # ['', 'a', '.', 'b', '..', '..', 'c', '.', 'd', 'e', '']
-        for i in carpetas:
-            if i == '':
+        res = "/" #El path de respuesta siempre empezará con esto
+        carpetas = path.split('/') #Obtenemos las carpetas haciendo splits en todo el string, carpetas serán las que están en medio de /
+        for i in carpetas: #Iteramos sobre la lista de carpetas
+            if i == '': #Cadena vacía (inicio y final)
                 pass
-            elif i == '.':
+            elif i == '.': #Nos quedamos en la misma carpeta
                 pass
-            elif i == '..':
-                res = res[:-1]
+            elif i == '..': #Regresamos una carpeta hacia arriba
+                res = res[:-1] #Quitamos el primer /, este es donde estaba "/.."
                 char = '1'
                 i = len(res)-1
                 aux = len(res)-1
-                print("El valor de res es: " + res + "\n.El valor de i es: " + str(i))
                 
-                while char != '/':
+                while char != '/': #Mientras que no encontremos otro / iteraremos de derecha a izquierda para obtener la carpeta completa, dicha carpeta será en la cual subimos un nivel
                     char = res[i]
                     i -= 1
 
-                aRestar = aux -i
-                res = res[:-aRestar+1]
+                aRestar = aux -i #Obtenemos la cantidad de caracteres que fue la carpeta
+                res = res[:-aRestar+1] #Y restamos esa cantidad al string de respuesta
 
-            else:
-                res += i
-                res += '/'
-        res = res[:-1]
-        if res == '':
+            else: #Llegamos aquí si en la iteración estamos en una carpeta (i.e. /Downloads)
+                res += i #Metemos la carpeta al path
+                res += '/' #Agregamos un / para la siguiente carpeta
+        res = res[:-1] #Cuando ya no hay carpetas, eliminamos ese último / que metimos en la línea 25
+        if res == '': #Si llegamos a root res estará vacío, devolvemos /
             return '/'
         else:
-            return res
+            return res 
 
             
 
