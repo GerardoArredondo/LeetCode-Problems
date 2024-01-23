81fec07d87b26ff38b8acb80c0855cf7663c192a
Gerardo <gerardoijgv11@gmail.com>
2024-01-23 15:55:26 -0600

    Added functions to the class

--- LinkedList.py
+++ LinkedList.py
@@ -24,12 +24,50 @@ class LinkedList:
         self.len +=1
 
 
+    #  0  1  2  3  4  5  6 <-- indexes
+    # 10 11 12 13 14 15 16
+    #  1  2  3  4  5  6  7 <-- positions
+
     def addAtIndex(self, value, index):
+        nodoAAgregar = Node(value)
+        current = self.head
         if index > (self.len - 1):
             print("No hay suficiente espacio")
-        
+            return
+        elif index == 0:
+            self.addAtBegin(value)
+            
+            return
+        else:
+            for i in range(index - 1):
+                current = current.next
+            nodoAAgregar.next = current.next
+            current.next = nodoAAgregar
+            self.len += 1
 
         
+            
+
+
+    def deleteHead(self):
+        if self.head == None and self.tail == None:
+            return
+        self.head = self.head.next
+        self.len -= 1
+
+
+
+    def deleteTail(self):
+        if self.tail == None:
+            return
+        else:
+            current = self.head
+            while current.next != self.tail:
+                current = current.next
+            current.next = None
+            self.tail = current
+            self.len -= 1
+
 
 
 
@@ -43,11 +81,6 @@ class LinkedList:
             print(current.value, end=" ")
             current = current.next
         print("")
-    
-    def deleteHead(self):
-        if self.head == None and self.tail == None:
-            return
-        self.head = self.head.next
             
         
         
@@ -74,5 +107,11 @@ head.imprimirLinkedList()
 head.addAtBegin(1000)
 head.imprimirLinkedList()
 head.deleteHead()
+head.imprimirLinkedList()
+
+head.addAtIndex(10000, 6)
+head.imprimirLinkedList()
+
+head.deleteTail()
 head.imprimirLinkedList()
         
